825e1183be8467bea48e5c74db229cbb61379e14
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2023-10-18 05:43:25 -0300

    Initial Commit

---
+++ PrimeiroAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Rodrigo Cerqueira Reis on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
