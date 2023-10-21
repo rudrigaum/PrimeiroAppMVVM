cdeabc7a932c1914f2a23736d0be9cba2eaad036
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2023-10-21 07:56:05 -0300

    First Commmit: Project Configuration

--- PrimeiroAppMVVM/HomeVC.swift
+++ PrimeiroAppMVVM/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  PrimeiroAppMVVM
 //
 //  Created by Rodrigo Cerqueira Reis on 18/10/23.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
